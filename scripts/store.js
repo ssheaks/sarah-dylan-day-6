53047601a110ec928cc2b6a8a02e9d2151b658ee
ssheaks <sarah.sheaks@gmail.com>
2018-02-26 11:33:44 -0800

    Update store to create, update, delete items

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global cuid */
+/* global cuid, Item */
 
 // eslint-disable-next-line no-unused-vars
 const store = (function() {
@@ -14,11 +14,46 @@ const store = (function() {
 
   let hideCheckedItems = false;
   let searchTerm = '';
+  const findById = function(id) {
+    return items.find(item => item.id ===id);
+  };
+
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.unshift(Item.create(name));
+    } catch(e) {
+      console.log(`Cannot add item ${e.message}`);
+    }
+  };
+
+  const findAndToggleChecked = function(id) {
+    let item = this.findById(id);
+    item.checked = !item.checked;
+  };
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch(e) {
+      console.log(`Cannot update name ${e.message}`);
+    }
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
 
   return {
     items, 
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
   
 }());
